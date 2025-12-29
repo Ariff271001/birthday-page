b6502c1b6073d0108d33210354ed31d5999285a8
Ariff Haikal <76397403+Ariff271001@users.noreply.github.com>
2025-12-29 13:29:11 +0800

    Change from animated to tap

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('#gift-img1').addEventListener('click', function() {
+  this.classList.toggle('animated');
+});
+document.querySelector('#gift-img2').addEventListener('click', function() {
+  this.classList.toggle('animated');
+});
